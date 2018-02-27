26fcc9883a0eee0db14b62fff8814c0185bb1f77
Dochko0 <dochko_dochev@yahoo.com>
2018-02-27 18:02:15 +0200

    List, LinkedList

---
+++ 02_LinearDataStructures_StacksAndQueues/a_CreateNode/a_CreateNode/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a_CreateNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> head = new Node<int>();
+            Node<int> node1 = new Node<int>();
+            head.Next = node1;
+        }
+    }
+    class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+    }
+}
