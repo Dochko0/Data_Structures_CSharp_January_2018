b1dd7ba61ca5d2b3ed694e0cc05b3d499d6ffaf9
Dochko0 <dochko_dochev@yahoo.com>
2018-03-01 17:34:59 +0200

    More Lab

---
+++ 02_LinearDataStructures_StacksAndQueues/b_MatchingBrackers/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string expression = "1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5";
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char curr = expression[i];
+
+                if (curr=='(')
+                {
+                    stack.Push(i);
+                }
+                else if (curr==')')
+                {
+                    int prev = stack.Pop();
+                    int lenght = i - prev+1;
+                    string exp = expression.Substring(prev, lenght);
+                    Console.WriteLine(exp);
+                }
+            }
+
+        }
+    }
+}
---
+++ 02_LinearDataStructures_StacksAndQueues/c_Sequence/c_Sequence/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            string[] input = Console.ReadLine().Split();
+            int number = int.Parse(input[0]);
+            int desiredNumber = int.Parse(input[1]);
+
+            queue.Enqueue(number);
+            int currIndex = 0;
+            while (queue.Count>0)
+            {
+                int curr = queue.Dequeue();
+                int increment = curr + 1;
+                int multi = curr * 2;
+                queue.Enqueue(increment);
+                queue.Enqueue(multi);
+                currIndex++;
+                if (curr==desiredNumber)
+                {
+                    Console.WriteLine(currIndex);
+                    break;
+                }   
+            }
+        }
+    }
+}
