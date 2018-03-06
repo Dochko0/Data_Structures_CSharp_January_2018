12c8bd58c6988e2082188fe216e4a03db5789754
Dochko0 <dochko_dochev@yahoo.com>
2018-03-06 13:02:38 +0200

    AVL tree

--- 06_LinearDataStr_HeapsAndPriorityQueue/BinaryHeap/BinaryHeap.cs
+++ 06_LinearDataStr_HeapsAndPriorityQueue/BinaryHeap/BinaryHeap.cs
@@ -7,29 +7,99 @@ public class BinaryHeap<T> where T : IComparable<T>
 
     public BinaryHeap()
     {
-        // TODO
+        this.heap = new List<T>();
     }
 
     public int Count
     {
         get
         {
-            throw new NotImplementedException();
+            return this.heap.Count;
         }
     }
 
     public void Insert(T item)
     {
-        throw new NotImplementedException();
+        this.heap.Add(item);
+        this.HeapifyUp(item, this.heap.Count - 1);
+    }
+
+    private void HeapifyUp(T item, int index)
+    {
+        int parent = (index - 1) / 2;
+        //index is a child
+        if (parent < 0)
+        {
+            return;
+        }
+        int compare = this.heap[parent].CompareTo(this.heap[index]);
+
+        if (compare < 0)
+        {
+            this.Swap(parent, index);
+            this.HeapifyUp(this.heap[parent], parent);
+        }
+    }
+
+    private void Swap(int parent, int index)
+    {
+        T temp = this.heap[parent];
+        this.heap[parent] = this.heap[index];
+        this.heap[index] = temp;
     }
 
     public T Peek()
     {
-        throw new NotImplementedException();
+        if (this.heap.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+        return this.heap[0];
     }
 
     public T Pull()
     {
-        throw new NotImplementedException();
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+        T element = this.heap[0];
+        this.Swap(0, this.Count - 1);
+        this.heap.RemoveAt(this.Count - 1);
+        this.HeapifyDown(0);
+
+        return element;
+    }
+
+    private void HeapifyDown(int index)
+    {
+        //int leftChild = (index * 2) + 1;
+        //int rightChild = leftChild + 1;
+        int parentIndex = index;
+
+        while (parentIndex < this.Count / 2)
+        {
+            //left child
+            int childIndex = (parentIndex * 2) + 1;
+            //check there is right child && rightChild > leftChild
+            if (childIndex + 1 < this.Count && isGreater(childIndex, childIndex + 1))
+            {
+                // RightChild
+                childIndex += 1;
+            }
+
+            int compare = this.heap[parentIndex].CompareTo(this.heap[childIndex]);
+            if (compare < 0)//parent < child
+            {
+                this.Swap(childIndex, parentIndex);
+                
+            }
+            parentIndex = childIndex;
+        }
+    }
+
+    private bool isGreater(int left, int right)
+    {
+        return this.heap[left].CompareTo(this.heap[right]) < 0;
     }
 }
--- 06_LinearDataStr_HeapsAndPriorityQueue/BinaryHeap/Heap.cs
+++ 06_LinearDataStr_HeapsAndPriorityQueue/BinaryHeap/Heap.cs
@@ -4,6 +4,78 @@ public static class Heap<T> where T : IComparable<T>
 {
     public static void Sort(T[] arr)
     {
-        throw new NotImplementedException();
+        ConstructHeap(arr);
+        HeapSort(arr);
+    }
+
+    private static void HeapSort(T[] arr)
+    {
+        for (int i = arr.Length-1; i >=0; i--)
+        {
+            Swap(arr, 0, i);
+            HeapifyDown(arr, 0, i);
+        }
+    }
+
+    private static void ConstructHeap(T[] arr)
+    {
+        for (int i = arr.Length / 2; i >= 0; i--)
+        {
+            HeapifyDown(arr, i, arr.Length);
+        }
+    }
+
+    //private static void HeapifyUp(T[] heap, T item, int index)
+    //{
+    //    int parent = (index - 1) / 2;
+    //    //index is a child
+    //    if (parent < 0)
+    //    {
+    //        return;
+    //    }
+    //    int compare = heap[parent].CompareTo(heap[index]);
+
+    //    if (compare < 0)
+    //    {
+    //        Swap(heap, parent, index);
+    //        HeapifyUp(heap, heap[parent], parent);
+    //    }
+    //}
+
+    private static void HeapifyDown(T[] arr, int index, int lenght)
+    {
+        //int leftChild = (index * 2) + 1;
+        //int rightChild = leftChild + 1;
+        int parentIndex = index;
+
+        while (parentIndex < lenght / 2)
+        {
+            //left child
+            int childIndex = (parentIndex * 2) + 1;
+            //check there is right child && rightChild > leftChild
+            if (childIndex + 1 < lenght && isGreater(arr, childIndex, childIndex + 1))
+            {
+                // RightChild
+                childIndex += 1;
+            }
+
+            int compare = arr[parentIndex].CompareTo(arr[childIndex]);
+            if (compare < 0)//parent < child
+            {
+                Swap(arr, childIndex, parentIndex);
+
+            }
+            parentIndex = childIndex;
+        }
+    }
+    private static bool isGreater(T[] heap, int left, int right)
+    {
+        return heap[left].CompareTo(heap[right]) < 0;
+    }
+    private static void Swap(T[] heap, int parent, int index)
+    {
+        T temp = heap[parent];
+        heap[parent] = heap[index];
+        heap[index] = temp;
     }
 }
